9e7b5dde6cc51dc13271c5aba641f2e986935973
h.zoo <134916809+hyunJuS2@users.noreply.github.com>
2025-01-20 17:43:10 +0900

    Comment: test를 위해 파일이름 변경 및 주석처리

---
+++ springboot-backend/src/main/java/com/github/aivle6th/ai23/springboot_backend/config/SecurityConfig_h.java
@@ -0,0 +1,36 @@
+//package com.github.aivle6th.ai23.springboot_backend.config;
+//
+//import com.github.aivle6th.ai23.springboot_backend.service.CustomUserDetailService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@EnableWebSecurity          // Spring Security 활성화 하고 웹 보안 설정을 구성하는데 사용하는 Annotation
+//@Configuration              // 해당 클래스가 @Bean 메서드를 포함하고 있으며, 스프링 컨테이너에 의해 빈 정의를 생성하는 데 사용할 수 있다.
+//public class SecurityConfig_h {
+//
+//    @Autowired
+//    private CustomUserDetailService userDetailService;
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/join", "/jointest").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .csrf(csrf -> csrf.disable())       // for test
+//                // .formLogin(Customizer.withDefaults());
+//                .formLogin(form -> form
+//                        .usernameParameter("employeeId")
+//                        .passwordParameter("pwd")
+//                        .loginProcessingUrl("/login")
+//                        .defaultSuccessUrl("/", true)
+//                );
+//        return http.build();
+//    }
+//}
+//
