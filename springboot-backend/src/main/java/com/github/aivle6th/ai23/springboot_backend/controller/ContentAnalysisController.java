8f519926ef93cf454a6a91ac6adcf0d8cec2dc9d
SJeongheon <fight0105@gmail.com>
2025-02-03 01:31:01 +0900

    Refactor : API endpoint 수정(/api/{boardId}/{postId} -> /api/{postId}) Feat : Add preauthorization

--- springboot-backend/src/main/java/com/github/aivle6th/ai23/springboot_backend/controller/ContentAnalysisController.java
+++ springboot-backend/src/main/java/com/github/aivle6th/ai23/springboot_backend/controller/ContentAnalysisController.java
@@ -13,10 +13,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
-@RequestMapping("/api/{boardId:\\d+}/{postId:\\d+}")
+@RequestMapping("/api/{postId:\\d+}")
 @RequiredArgsConstructor
 @Tag(name = "Content Analysis API", description = "게시물 분석 결과 관리 API")
 public class ContentAnalysisController {
@@ -24,6 +25,7 @@ public class ContentAnalysisController {
     private final ContentAnalysisService contentAnalysisService;
 
     @Operation(summary = "분석 결과 조회", description = "특정 게시물에 대한 분석 결과를 조회합니다.")
+    @PreAuthorize("@securityService.canAccessPost(authentication, #postId)")
     @GetMapping("/content-analysis")
     public ResponseEntity<ApiResponseDto<ContentAnalysisResponseDto>> getContentAnalysis(
             @Parameter(description = "게시물 ID", required = true) @PathVariable Long postId) {
@@ -33,6 +35,7 @@ public class ContentAnalysisController {
 
     @Operation(summary = "분석 결과 생성", description = "특정 게시물에 대한 분석 결과를 생성합니다.")
     @PostMapping("/content-analysis")
+    // TODO AI 서버에서만 호출 가능하도록 설정하기
     public ResponseEntity<ApiResponseDto<ContentAnalysisResponseDto>> createContentAnalysis(
             @RequestBody ContentAnalysisCreateRequestDto requestDto,
             @Parameter(description = "게시물 ID", required = true) @PathVariable Long postId) {
